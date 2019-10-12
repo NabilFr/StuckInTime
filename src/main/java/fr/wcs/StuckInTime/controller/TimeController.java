e74750316ea698dc4e774bc57e981fe0618d99a9
Marie Vendran <marie.vendran@gmail.com>
2019-10-12 15:43:45 +0200

    attaques de Joker Pika et The mask ajoutés, corrections

--- src/main/java/fr/wcs/StuckInTime/controller/TimeController.java
+++ src/main/java/fr/wcs/StuckInTime/controller/TimeController.java
@@ -10,11 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TimeController {
-
-    private Hero personnage1 = new Hero("default","default","default");
+    String[] heroes = {"Joker", "Pika", "The Mask"};
+    private Hero personnage1 = new Hero("","","");
+    static String name = "";
 
     @PostMapping("/poneymagique")
-    public String indexContinue(@RequestParam(name = "continue") int choice) {
+    public String indexContinue(Model model, @RequestParam(name = "continue") int choice) {
+
+           personnage1.setName(heroes[choice]);
+           name = personnage1.getName();
+
+        model.addAttribute("name", name);
 
         return "/weapon";
     }
@@ -23,17 +29,25 @@ public class TimeController {
     public String weaponChoice(Model model, @RequestParam(name = "weapon") int weapon) {
 
         String[] arme = {"épée", "fourchette", "Bouclier"};
-
+        model.addAttribute("name", name);
         model.addAttribute("arme", arme[weapon]);
         personnage1.setArme(arme[weapon]);
-        return "/attack";
+        if (personnage1.getName().equals("Joker")) {
+            return "/attackJoker";
+        }
+        else if(name.equals("Pika")) {
+            return "/attackPika";
+        }
+        else {
+            return "/attackTheMask";
+        }
     }
 
     @PostMapping("/win")
     public String attackChoices(Model model, @RequestParam(name = "attack") int attack) {
 
         String[] attackChoice = {"frontale", "dans l'oeil", "insulte"};
-
+        model.addAttribute("name", name);
         model.addAttribute("attack", attackChoice[attack]);
         personnage1.setAttack(attackChoice[attack]);
         if (personnage1.getAttack().equals("frontale") && personnage1.getArme().equals("fourchette")) {

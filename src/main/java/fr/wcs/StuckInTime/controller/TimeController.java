5f507e77b949ea1b17592790ac635fb36226a376
Morgane M <morgane.marie4602@gmail.com>
2019-10-13 11:11:24 +0200

    Attaque du Mask

--- src/main/java/fr/wcs/StuckInTime/controller/TimeController.java
+++ src/main/java/fr/wcs/StuckInTime/controller/TimeController.java
@@ -74,6 +74,41 @@ public class TimeController {
                 ending = "JC dépose l'épée de légende au sol pour essayer de convaincre le géant que la paix is the best way. Malheureusement ça ne prend pas, le géant connaît bien JCVD et l'a vu castagner un tas d'ennemis dans ses nombreux films, il ne lui fait donc pas confiance. Il saisit l'épée et vous embroche tous les deux avant de vous déposer sur son tourne-broche.";
                 personnage1.setScore(-20);
                 break;
+
+
+
+            case "[4, 0, 0]":
+                ending = "Le Mask agite la baguette, qui commence à briller. Alors qu'il exécute un magnifique flamenco avec la baguette entre les dents, le monstre finit par s'impatienter et l'écrase. C'était une belle prestation, mais un échec cuisant. Un petit « splendide... » peut être entendu depuis le dessous du pied du monstre.";
+                personnage1.setScore(5);
+                break;
+            case "[4, 0, 1]":
+                ending = "Le Mask fait quelques étirements, craque sa nuque, puis, avec de la peinture de guerre étalée sur les joues, il court en hurlant un cri de guerre en direction du monstre... Sauf qu'il trébuche sur une racine et se la plante dans l’œil. C'était bien tenté. Quel dommage qu'il y ait eu cette racine au milieu de la prairie...";
+                personnage1.setScore(5);
+                break;
+            case "[4, 1, 0]":
+                ending = "Le Mask prend la pose d'un lanceur de baseball, levant souplement la jambe devant lui qui exécutant une rotation contrôlée du torse. La pierre fuse en direction du ciel, et s'éloigne... et s'éloigne... « HOME RUN !!! » hurle le Mask en partant en courant, jetant une casquette dont vous ignorez la provenance dans un public invisible. Vous auriez dû l'avertir que vous n'étiez pas en pleine partie de baseball... Il est partis courir les bases.";
+                personnage1.setScore(-5);
+                break;
+            case "[4, 1, 1]":
+                ending = "Le Mask fait apparaître un canon digne des plus grands pirates des siècles derniers. Sous votre œil médusé, il enfonce la pierre à l'intérieur et il allume la mèche, s'accroupissant derrière l'énorme pièce de métal en se bouchant les oreilles.
+« BAAM!! » La pierre siffle dans l'air, alors qu'elle fuse en une jolie courbe en direction du monstre.\n
+Vous ne savez pas trop comment le Mask s'y est prit, mais il semblerait que le point faible du monstre était son petit orteil droit ; il tombe raide mort avec un grognement de douleur et de surprise.
+\n
+… Il tombe sur vous. Dommage.";
+                personnage1.setScore(20);
+                break;
+            case "[4, 2, 0]":
+                ending = "Le Mask glisse un main derrière son dos, de jolies moustaches apparaissant au dessus de sa bouche, ainsi qu'un masque noir sur ses yeux. Cette pose est élégante... Certes... Mais pas très pertinente face à un monstre de vingt mètres. Nous ne sommes pas dans un combat singulier. Le Mask se fait déchirer en deux pas le monstre. \n Et devinez quoi ? Vous êtes le suivant.";
+                personnage1.setScore(-20);
+                break;
+            case "[4, 2, 1]":
+                ending = "Le Mask se saisit de l'épée, la brandissant en l'air dans une pose héroïque. Le soleil se reflète sur la lame et éblouit le monstre, qui trébuche en arrière en se couvrant les yeux. Finalement, dans la panique pour se rattraper, et ayant les mains occupées, le monstre s'écrase en avant... Droit sur le Mask. Votre héros s'enfonce dans la narine du monstre, et après un long moment silencieux, il en ressort en crachotant un peu de morve. Vous... Vous avez gagné ?!
+\n
+Vous vous approchez du Mask pour le féliciter... Puis vous remarquez le détonateur dans sa main droite. Qu'est-ce que... ? 
+\n
+« BOOM ! » Ouais... Il a fait exploser le monstre.. Et vous avec.";
+                personnage1.setScore(-20);
+                break;
         }
         model.addAttribute("ending", ending);
         return "/win";
